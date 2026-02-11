1dc04db35b8adda3f2b8a1ed47cb83ac2a7f5f3d
Cybertechyrappu <157779011+Cybertechyrappu@users.noreply.github.com>
2026-02-11 14:25:24 +0000

    yes

--- main.js
+++ main.js
@@ -134,7 +134,7 @@ document.body.innerHTML = `
                 <span class="hacker-text" data-value="SAYYID RAFID">SAYYID RAFID</span><br>
                 <span class="hacker-text" data-value="AL HADI">AL HADI</span>
             </h1>
-            <p>Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.</p>
+            <p style="text-align: center; >Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.</p>
             <a href="#work" class="btn">View Projects</a>
         </section>
         
